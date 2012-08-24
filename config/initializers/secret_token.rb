36876f055494216ec02c2223c8e7e2a4c9dae788
Sarah Adams <sarahsponda902@gmail.com>
2012-08-23 18:09:54 -0700

    G

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookshop::Application.config.secret_token = '8ef1e368e357049d2d4307809bb45eea31a0fed77c51565561b71e8721cd3a29945cf8278fff825076f6e1bfa24c2b3cba0ef4bc9e7d51842080e4f4165a9a12'
